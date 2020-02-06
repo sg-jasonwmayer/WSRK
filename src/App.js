c124530087098999bf46220c99edfc7ca2bfff95
Jason Mayer <flavorflavor@gmail.com>
2020-02-06 11:13:16 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import ApiTest from './components/Header/ApiTest';
+
+const App = () => {
+  return (
+    <div> 
+      <Header />
+      <ApiTest />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/temp.js
@@ -0,0 +1,35 @@
+import { Menu, MenuItem, Fade, Grid, IconButton } from '@material-ui/core';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+    },
+}));
+
+export default function UserName() {
+    const classes = useStyles();
+    return (
+        <div className='classes.root'>
+
+        </div>
+    )
+    import { Menu, MenuItem, Fade, Grid, IconButton } from '@material-ui/core';
+    import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+    import { makeStyles } from '@material-ui/core/styles';
+    import clsx from 'clsx';
+    
+    const useStyles = makeStyles(theme => ({
+        root: {
+        },
+    }));
+    
+    export default function UserName() {
+        const classes = useStyles();
+        return (
+            <div className='classes.root'>
+    
+            </div>
+        )
+    }
