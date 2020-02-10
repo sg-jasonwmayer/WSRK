2c2b84392411bacf292d42e1b0c3c66268363c80
Felipe <felhenao@gmail.com>
2020-02-10 15:20:41 -0500

    added missing values to radio lables on ColumnsToInclude

--- src/components/LoadPlanner/ColumnsToInclude.js
+++ src/components/LoadPlanner/ColumnsToInclude.js
@@ -29,13 +29,13 @@ const ColumnsToInclude = () => {
                 <RadioGroup aria-label="" name="" value={value} onChange={handleChange}>
                     <FormControlLabel value="IncludeBoth" control={<Radio />} label="Interfaced Date" />
                     <FormControlLabel value="OptimizedOnly" control={<Radio />} label="Interfaced User" />
-                    <FormControlLabel  control={<Radio />} label="Optimized User" />
-                    <FormControlLabel  control={<Radio />} label="IMill/Cust Preference" />
-                    <FormControlLabel  control={<Radio />} label="Last Vehicle Wt % Utilized" />
-                    <FormControlLabel  control={<Radio />} label="Last Vehicle Vol % Utilized" />
-                    <FormControlLabel  control={<Radio />} label="Total Rolls" />
-                    <FormControlLabel  control={<Radio />} label="Total Weight" />
-                    <FormControlLabel  control={<Radio />} label="Load Plan Name" />
+                    <FormControlLabel  value="OptimizedUser" control={<Radio />} label="Optimized User" />
+                    <FormControlLabel  value="CustPreference" control={<Radio />} label="IMill/Cust Preference" />
+                    <FormControlLabel  value="LastVehicleWT" control={<Radio />} label="Last Vehicle Wt % Utilized" />
+                    <FormControlLabel  value="LastVehicleVol" control={<Radio />} label="Last Vehicle Vol % Utilized" />
+                    <FormControlLabel  value="TotalRolls" control={<Radio />} label="Total Rolls" />
+                    <FormControlLabel  value="TotalWeight" control={<Radio />} label="Total Weight" />
+                    <FormControlLabel  value="LoadPlanName" control={<Radio />} label="Load Plan Name" />
                 </RadioGroup>
             </FormControl>
         </div>   
