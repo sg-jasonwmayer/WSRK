040910bf0210f2eb3a6c9a6090f48a38f1011183
Jason Mayer <flavorflavor@gmail.com>
2020-02-15 17:20:38 -0500

    updates to everything

---
+++ src/containers/MillOne/MillOne.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './MillOne.scss';
+
+const MillOne = () => {
+    return (
+      <div> 
+       <h2>Mill One</h2> 
+      </div>
+    );
+  }
+  
+
+export default MillOne;
