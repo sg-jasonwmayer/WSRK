dd9983a2f97bb9455b7b70949d23ee7ce4f5ec26
Felipe Henao <felipe.henao@westrock.com>
2020-02-16 16:50:48 -0500

    added Delete icon to ValueInputBox

--- src/components/ValueInputBox.js
+++ src/components/ValueInputBox.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -20,7 +21,11 @@ const ValueInputBox = () => {
       <Button variant="contained" href="#contained-buttons">
         Apply Criteria
       </Button>
-       <Button variant="contained" href="#contained-buttons">
+       <Button 
+       variant="contained"
+       color="secondary"
+       startIcon={<DeleteIcon />}
+       href="#contained-buttons">
         Discard
       </Button>
       <form className={classes.root} noValidate autoComplete="off">
