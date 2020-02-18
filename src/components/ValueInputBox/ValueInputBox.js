d91aec765ba8f3096ea9809e6d0d3a363f0f8ae3
Jason Mayer <flavorflavor@gmail.com>
2020-02-18 14:00:58 -0500

    componenets, styling

---
+++ src/components/ValueInputBox/ValueInputBox.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  TextField,
+  Button}
+   from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import './ValueInputBox.scss';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: 200,
+      },
+    },
+  }));
+
+const ValueInputBox = () => {
+  const classes = useStyles();
+
+  return(
+    <div className="root" >
+      <Button    className="value-button" variant="contained" href="#contained-buttons">
+        Apply Criteria
+      </Button>
+       <Button
+       className="value-button"
+        variant="contained"
+        color="secondary"
+        startIcon={<DeleteIcon />}
+        href="#contained-buttons">
+        Discard
+      </Button>
+      <form className='value-input' noValidate autoComplete="off">
+          <TextField id="outlined-basic" label="Value" variant="outlined" />
+      </form>
+    </div>
+  )
+}
+
+export default ValueInputBox;
