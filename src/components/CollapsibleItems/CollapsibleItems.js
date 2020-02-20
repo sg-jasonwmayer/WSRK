0f208401984a0e84e28cc0c77bedd2d392b15098
Felipe Henao <felipe.henao@westrock.com>
2020-02-20 14:34:24 -0500

    Cleaned up CollapsibleItems

--- src/components/CollapsibleItems/CollapsibleItems.js
+++ src/components/CollapsibleItems/CollapsibleItems.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import './CollapsibleItems.scss';
 
@@ -14,45 +13,26 @@ import ValueInputBox from '../ValueInputBox/ValueInputBox.js';
 import Collapsible from 'react-collapsible';
 
 const CollapsibleItems = () => {
-
-
-//   // eslint-disable-next-line no-unused-vars
-//   const handleClickCollapsible = (e) => {
-//     e.preventDefault();
-
-// }
-  
   return (
-    
     <div className="value-input">
-     <ValueInputBox />
-     <Collapsible  trigger="Criteria">
-       <CriteriaDropDown /> 
-     </Collapsible><Collapsible trigger="Filter Search By">
-         <FilterSearchBy /> 
-         </ Collapsible><Collapsible trigger="Filter by Mode">
-             <FilterByMode />
-              </Collapsible>
-              <Collapsible trigger="Optimized Filters">
-              </Collapsible>
-              <Divider />
-              <Collapsible trigger="Columns to Include in Selection Data Grid">
-              <ColumnsToInclude /> 
-              </Collapsible> 
-              <Divider />
-              <Collapsible trigger="Filter by Mode">
-                <FilterByMode /> 
-                  </ Collapsible>
-                  <Divider />
-                  <Collapsible trigger="Optimized Filters">
-                       <OptimizedFilter />
-                        </ Collapsible> 
-                        <Collapsible trigger="Columns to Include in Selection Data Grid"> 
-                        <ColumnsToInclude /> 
-                        </Collapsible>
-                        </div>
+      <ValueInputBox />
+      <Collapsible  trigger="Criteria">
+        <CriteriaDropDown /> 
+      </Collapsible>
+      <Collapsible trigger="Filter Search By">
+        <FilterSearchBy /> 
+      </Collapsible>
+      <Collapsible trigger="Filter by Mode">
+        <FilterByMode />
+      </Collapsible>
+      <Collapsible trigger="Optimized Filters">
+        <OptimizedFilter />
+      </Collapsible>
+      <Collapsible trigger="Columns to Include in Selection Data Grid">
+        <ColumnsToInclude /> 
+      </Collapsible> 
+    </div>
   );
 }
 
 export default CollapsibleItems;
-
