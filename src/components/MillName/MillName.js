526450cb897028126df14e263d09adbfd0543517
Jason Mayer <flavorflavor@gmail.com>
2020-02-26 14:43:44 -0500

    mill name

--- src/components/MillName/MillName.js
+++ src/components/MillName/MillName.js
@@ -1,7 +1,5 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
 import React from 'react';
-import logo from '../../assets/logo-westrock.svg'
 
 import './MillName.scss'
 
