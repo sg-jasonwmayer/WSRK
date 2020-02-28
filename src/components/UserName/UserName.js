327ff599cbadc63e24670b6998ca905977d584f6
Felipe Henao <felipe.henao@westrock.com>
2020-02-27 19:17:18 -0500

    added user preferences funcionality

--- src/components/UserName/UserName.js
+++ src/components/UserName/UserName.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import { 
   IconButton, 
   Menu, 
-  MenuItem
+  MenuItem,
+  FormControl,
+  InputLabel,
+  NativeSelect
 } from '@material-ui/core';
 
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import { makeStyles } from '@material-ui/core/styles';
 
 import axios from 'axios';
 
@@ -15,23 +19,63 @@ import './UserName.scss'
 const fhenao = 'http://ms00015080d:7050/api/userpreferences/mill/fhenao';
 
 let userName = '';
-let units = '';
 
 axios.get(fhenao).then((res) => {
   userName = res.data.userName;
-  units = res.data.units;
 })
   .catch((err) => {
       console.log(err);
   })
   // console.log(userName);
-  // console.log(units);
 
+  const useStyles = makeStyles(theme => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 190,
+    },
+  }));
+  
+const UserName = () => {
+  const classes = useStyles();
 
-export default function UserName() {
+  const [ userPrefState, setUserPrefState ] = useState({
+    UserPreferences: [
+      {userName: "fhenao"},
+      {lX_UserFullName: "fhenao"},
+      {language: "English"},
+      {units: "Imperial"},
+      {mill_group: "Panther"},
+      {web_role: "Admin"},
+      {lx_role: "Admin"},
+      {include_advisecase: false},
+      {lx_system: "Mill"}
+    ]
+  });
 
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const opens = Boolean(anchorEl);
+  const open = Boolean(anchorEl);
+
+  const [unitState, setUnitState] = React.useState({
+    type: '',
+  });
+
+  const handleUnitChange = type => event => {
+    setUnitState({
+      ...unitState,
+      [type]: event.target.value,
+    });
+  };
+
+  const [languageState, setLanguageState] = React.useState({
+    language: '',
+  });
+
+  const handleLanguageChange = language => event => {
+    setLanguageState({
+      ...languageState,
+      [language]: event.target.value,
+    });
+  };
 
   const handleClick = event => {
     setAnchorEl(event.currentTarget);
@@ -41,53 +85,79 @@ export default function UserName() {
     setAnchorEl(null);
   };
   return (
-      <div className='root'>
-          <IconButton
-              size='small'
-              aria-controls='fade-menu' 
-              aria-haspopup='true' 
-              onClick={handleClick}
-          >
-              <ArrowDropDownIcon 
-                color='primary' 
-              />
-              <h5>
-                {userName}
-              </h5> 
-          </IconButton> 
-          <Menu
-            keepMounted
-            id='fade-menu'
-            anchorEl={anchorEl}
-            open={opens}
-            onClose={handleClose}
-          >
-              <MenuItem 
-                onClick={handleClose}
-              >
-                Language: English
-                </ MenuItem>
-                <MenuItem 
-                onClick={handleClose}
-              >
-                Mill Group: Panther
-                </ MenuItem>
-                <MenuItem 
-                onClick={handleClose}
-              >
-                Web Role: Admin
-                </ MenuItem>
-                <MenuItem 
-                onClick={handleClose}
-              >
-                Units: Imperial
-                </ MenuItem>
-              <MenuItem 
-                onClick={handleClose}
-                units={units}
-              />
-              
-          </ Menu>
-      </div>
+    <div className='root'>
+      <IconButton
+          size='small'
+          aria-controls='fade-menu' 
+          aria-haspopup='true' 
+          onClick={handleClick}
+      >
+        <ArrowDropDownIcon 
+          color='primary' 
+        />
+        <h5>
+          {userName}
+        </h5> 
+      </IconButton> 
+      <Menu
+        keepMounted
+        id='fade-menu'
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem 
+        onClick={handleClose}
+        >
+          Include AdviseCase:  
+          {userPrefState.UserPreferences[7].include_advisecase.toString()}
+          
+        </ MenuItem>
+        <MenuItem 
+        >
+          <FormControl className={classes.formControl} error>
+            <InputLabel htmlFor="name-native-error" className="inputLabel">Units</InputLabel>
+            <NativeSelect
+              value={unitState.type}
+              onChange={handleUnitChange('type')}
+              name="name"
+              inputProps={{
+                id: 'name-native-error',
+              }}
+            >
+              <option value="" />
+              <option value="English">
+                {userPrefState.UserPreferences[3].units}
+              </option>
+              <option>Metric</option>
+            </NativeSelect>
+          </FormControl>
+        </MenuItem>
+
+        <MenuItem 
+        >
+          <FormControl className={classes.formControl} error>
+            <InputLabel htmlFor="name-native-error">Language</InputLabel>
+            <NativeSelect
+              value={languageState.mode}
+              onChange={handleLanguageChange('language')}
+              name="name"
+              inputProps={{
+                id: 'name-native-error',
+              }}
+            >
+              <option value="" />
+              <option value="English">
+              {userPrefState.UserPreferences[2].language}
+              </option>
+              <option value="French">French</option>
+              <option value="Portuguese">Portuguese</option>
+            </NativeSelect>
+          </FormControl>
+        </MenuItem>
+      </ Menu>
+    </div>
   )
 }
+
+export default UserName;
