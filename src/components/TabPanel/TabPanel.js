9cb01ce23a586a73156c3bbd1abb21a4250865c2
Jason Mayer <flavorflavor@gmail.com>
2020-03-03 15:08:14 -0500

    TabPanel Component

---
+++ src/components/TabPanel/TabPanel.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+
+import './TabPanel.scss'
+
+
+const TabPanel = (props) => {
+  const { children, value, index, ...other } = props;
+  
+    return (
+      <Typography
+        component="div"
+        role="tabpanel"
+        hidden={value !== index}
+        id={`scrollable-auto-tabpanel-${index}`}
+        aria-labelledby={`scrollable-auto-tab-${index}`}
+        {...other}
+      >
+        <Box p={3}>{children}</Box>
+      </Typography>
+    );
+  }
+  
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+  };
+  
+  function a11yProps(index) {
+    return {
+      id: `scrollable-auto-tab-${index}`,
+      'aria-controls': `scrollable-auto-tabpanel-${index}`,
+    };
+  }
+
+  export default TabPanel;
---
+++ src/components/TabPanel/TabPanel.spec.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { render } from '@testing-library/react';
+import TabPanel from './TabPanel';
+
+test.skip('renders component', () => {
+  const { getByText } = render(<TabPanel />);
+  expect(getByText).toBeInTheDocument();
+});
