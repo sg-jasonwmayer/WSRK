85d7eb7e5bf211b37e56aeb5c6faf215521e0ad2
Jason Mayer <jason@jasonmayer.com>
2020-04-28 08:16:43 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header/Header';
+
+import './App.scss'
+import 'typeface-roboto';
+
+const App = () => {
+  return (
+    <div className="load-x-pert">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
