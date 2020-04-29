dc65d1e9d4fdc2a33b801656579a743fdce355cc
Jason Mayer <jason@jasonmayer.com>
2020-04-29 09:13:02 -0400

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import logo from '../../assets/logo.png';
+import './Header.scss';
+
+const Header = () => {
+  return (
+
+    <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default Header;
