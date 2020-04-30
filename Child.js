46a4bc47d227c20d5bb4d1c05e4abb051f1c5184
Jason Mayer <jason@jasonmayer.com>
2020-04-30 10:34:41 -0400

    Add files via upload

---
+++ Child.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {useParams} from "react-router-dom";
+  
+import './Child.scss';
+
+const Child = () => {
+    let { id } = useParams();
+    return (
+      <div>
+        <h3>ID: {id}</h3>
+      </div>
+      );
+    }
+
+    export default Child;
