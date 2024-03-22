7585f47e9964be8b17b3eb79e5648d0fbe405198
Jason W Mayer <flavorflavor@gmail.com>
2024-03-22 10:30:54 -0400

    Update App.spec.js

--- App.spec.js
+++ App.spec.js
@@ -1,19 +1,18 @@
 /* eslint-disable no-undef */
 describe('LoadXpert APP Test Suite', () => {
 
-    it('Boolean Equality Check', () => {
-      expect(true).toEqual(true);
-    });
-
-    it.skip('Should have the correct class', function () {
-      expect(hasClass(''), 'does have class ""').to.be.true;
-    });
-
-    it.skip('Should have the AppView container', function () {
-      expect(find()).to.have.lengthOf(1);
-    });
-
-
+  it('Boolean Equality Check', () => {
+    expect(true).toEqual(true);
   });
 
-   
+  it.skip('Should have the correct class', function () {
+    expect(hasClass(''), 'does have class ""').to.be.true;
+  });
+
+  it.skip('Should have the AppView container', function () {
+    expect(find()).to.have.lengthOf(1);
+  });
+
+
+});
+
